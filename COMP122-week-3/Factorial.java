7db4d4c819e0542ea381c641b1691900a6009bf2
taeevuu <alex.tilev420@gmail.com>
2026-02-16 11:51:50 +0000

    Shanty problem done

---
+++ COMP122-week-3/Factorial.java
@@ -0,0 +1,10 @@
+public class Factorial {
+    public static void main() {
+        int n = ;
+        factorial = ?;
+        for(? ; ? ; ?) {
+        }
+
+        System.out.println(factorial);
+    }
+}
---
+++ COMP122-week-3/Largest.java
@@ -0,0 +1,25 @@
+public class Largest {
+    public static void main() {
+        System.out.print("Length of Array:");
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int n = scanner.nextInt();
+
+        // your code here...
+        int[] myArray = ?
+
+        for (;;) {
+            ? = scanner.nextInt();
+        }
+
+        int largestValue = ?
+
+        for (;;) {
+            if (?) {
+                ?
+            }
+        }
+
+        // end of your code...
+        System.out.println(largestValue);
+    }
+}
