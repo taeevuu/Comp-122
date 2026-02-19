92709707cc8adc646ea75ece2f89f6a218ce1615
taeevuu <alex.tilev420@gmail.com>
2026-02-19 14:20:47 +0000

    final fix Newton

--- COMP122-week-3/Newton.java
+++ COMP122-week-3/Newton.java
@@ -1,21 +1,24 @@
 public class Newton {
   public static void main(String[] args) {
     // Read in values (assume valid positive inputs)
-    if (args.length != 2) {
-      System.out.println("Usage: java Newton <n> <guess>");
+    if (args.length != 2 && args.length != 3) {
+      System.out.println("Usage: java Newton <n> <guess> [tolerance]");
       return;
     }
 
     double n = Double.parseDouble(args[0]);
     double guess = Double.parseDouble(args[1]);
+    double tolerance = 0.0000001; // default original tolerance
+    if (args.length == 3) {
+      tolerance = Double.parseDouble(args[2]);
+    }
 
-    double result = sqRoot(n, guess);
+    double result = sqRoot(n, guess, tolerance);
     System.out.println(result);
   }
 
-  public static double sqRoot(double n, double guess) {
+  public static double sqRoot(double n, double guess, double tolerance) {
     if (n == 0) return 0.0;
-    double tolerance = 1e-15;
     double next;
     int maxIter = 1000;
     int iter = 0;
