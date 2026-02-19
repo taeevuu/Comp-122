1702a9e5135d6ba8d730b8b3983622b76a85f308
taeevuu <alex.tilev420@gmail.com>
2026-02-19 11:54:52 +0000

    finished factorial

--- COMP122-week-3/Factorial.java
+++ COMP122-week-3/Factorial.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 public class Factorial {
-    public static void main() {
-        int n = ;
-        factorial = ?;
-        for(? ; ? ; ?) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+        long factorial = 1;
+        for(int i = 1; i <= n; i++) {
+            factorial = factorial * i;
         }
-
         System.out.println(factorial);
+        scanner.close();
     }
 }
