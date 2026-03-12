3bb16ffda64aeaff3a55d8c897521d46b000b0e4
taeevuu <alex.tilev420@gmail.com>
2026-03-12 14:52:33 +0000

    retry completed

--- COMP108-portfolio-2/Student.java
+++ COMP108-portfolio-2/Student.java
@@ -2,7 +2,7 @@ public class Student {
     public boolean hasSubmitted = false;
     public String name  = "";
     public String email = ""; 
-    public int yearofBirth = 0; 
+    public int yearOfBirth = 0; 
     public int enrolmentYear = 0; 
     public int studentId = 0; 
     public int grade = 0;
